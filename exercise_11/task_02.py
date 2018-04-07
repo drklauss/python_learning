13107a7fa0c651cfa1262fca9938eab22c81ad7f
alog <dr.klauss@gmail.com>
2018-04-07 21:02:19 +0300

    11.2

--- exercise_11/task_02.py
+++ exercise_11/task_02.py
@@ -4,22 +4,69 @@
 # а также извлечь из этого числа квадратный корень
 # и взять тригонометрическую функцию. Написать еще один класс, кроме перечисленного имеющий одно свойство
 # и понимающий команды записать в память, извлечь из памяти, добавить x к содержимому памяти.
+import math
 
 
 class Memory:
     def __init__(self):
-        self._num = 0.00
+        self._mem = 0.00
 
     @property
-    def num(self):
-        return self._num
+    def mem(self):
+        return self._mem
 
-    @num.setter
-    def num(self, value):
-        self._num = value
+    @mem.setter
+    def mem(self, value):
+        self._mem = value
 
-    @num.deleter
-    def num(self):
-        del self._num
+    @mem.getter
+    def mem(self):
+        return self._mem
+
+    def mem_plus(self, val):
+        self._mem += val
+        return self
+
+    def mem_minus(self, val):
+        self._mem -= val
+        return self
+
 
 class Calculator(Memory):
+    def __init__(self):
+        super().__init__()
+        self.value = 0.00
+
+    def __str__(self):
+        return "{:.2f}".format(self.value)
+
+    def plus(self, val):
+        self.value += val
+        return self
+
+    def minus(self, val):
+        self.value -= val
+        return self
+
+    def mul(self, val):
+        self.value *= val
+        return self
+
+    def div(self, val):
+        self.value /= val
+        return self
+
+    def square_root(self):
+        self.value = pow(self.value, 0.5)
+        return self
+
+    def cos(self):
+        self.value = math.cos(self.value)
+        return self
+
+
+calc = Calculator()
+calc.mem = calc.value
+calc.mem_plus(10).mem_minus(5)
+print(calc)
+print(calc.mem)
